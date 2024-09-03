8ce604fd3c9d9534de8168454f68df5ff26fea3a
Wilian Zembrani <Wilian3355@gmail.com>
2024-09-03 20:15:44 -0300

    update

--- produto/views.py
+++ produto/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
-
+from . import models
 
 class ListaProdutos(ListView):
-    pass
+        model = models.Produto
+        template_name = 'produto/list.html'
 
 class DetalheProduto(View):
     pass
