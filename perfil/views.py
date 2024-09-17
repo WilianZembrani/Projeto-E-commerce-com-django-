e80de64ce1fb784db7bf8667e19f264479ab78b7
Wilian Zembrani <Wilian3355@gmail.com>
2024-09-16 21:09:44 -0300

    Atualizando a view Login e logout para logar e deslogar usuários, e atualizando templates

--- perfil/views.py
+++ perfil/views.py
@@ -143,7 +143,41 @@ class Atualizar(View):
     pass
 
 class Login(View):
-    pass
+    def post(self, *args, **kwargs):
+        username = self.request.POST.get('username')
+        password = self.request.POST.get('password')
+
+        if not username or not password:
+            messages.error(
+                self.request,
+                'Usuário ou senha inválidos.'
+            )
+            return redirect('perfil:criar')
+
+        usuario = authenticate(
+            self.request, username=username, password=password)
+
+        if not usuario:
+            messages.error(
+                self.request,
+                'Usuário ou senha inválidos.'
+            )
+            return redirect('perfil:criar')
+
+        login(self.request, user=usuario)
+
+        messages.success(
+            self.request,
+            'Você fez login no sistema e pode concluir sua compra.'
+        )
+        return redirect('produto:carrinho')
 
 class Logout(View):
-    pass
+    def get (self,*args, **kwargs):
+        carrinho = copy.deepcopy(self.request.session.get('carrinho'))
+        
+        logout(self.request)
+
+        self.request.session['carrinho'] = carrinho
+        self.request.session.save()
+        return redirect('produto:lista')
